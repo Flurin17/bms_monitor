96fce899e89ec0de76756cdfe224e6cf2e9bdd11
Flurin17 <53998860+Flurin17@users.noreply.github.com>
2022-09-20 17:04:29 +0000

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,5 @@
+mongoDbUrl = "mongodb+srv://test:test@cluster0.atqgn.mongodb.net/<dbname>?retryWrites=true&w=majority"
+webhookname = 'https://discord.com/api/webhooks//'
+
+username = "max.mustermann"
+password = "Test1234a"
